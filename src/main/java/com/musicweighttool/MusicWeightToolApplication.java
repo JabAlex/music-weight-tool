215dee4dd56c7c0a0b862cbe5553ae5e7e66a7a7
Aleksander Jablonski <jablonski.aleksander98@gmail.com>
2023-10-28 23:02:26 +0200

    first commit

---
+++ src/main/java/com/musicweighttool/MusicWeightToolApplication.java
@@ -0,0 +1,13 @@
+package com.musicweighttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicWeightToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicWeightToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicweighttool/MusicWeightToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicweighttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicWeightToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
